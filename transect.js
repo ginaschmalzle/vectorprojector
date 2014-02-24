0e471fb9ba66378134b28bbb1775049b8127252e
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-02-24 16:22:37 -0500

    This version prints dots within a transect

--- transect.js
+++ transect.js
@@ -45,23 +45,58 @@ function transect() {
                                 color: 'green',
                                 weight: 5
                         }).addTo(map);
-			}
-	});
-	};
 
-/*
+        		var myFilteredData = null;
+        		myFilteredData = $(myData).filter(function() {
+				var x = (mapclick.startLatlng.lng - mapclick.endLatlng.lng)*111;
+				var y = (mapclick.startLatlng.lat - mapclick.endLatlng.lat)*111;
+				var rx = (mapclick.startLatlng.lng - this.geometry.coordinates[0])*111;
+				var ry = (mapclick.startLatlng.lat - this.geometry.coordinates[1])*111;
+			//		console.log(rx, mapclick.startLatlng.lng, this.geometry.coordinates[0]);
+				var R = Math.sqrt((rx*rx) + (ry*ry));
+				var Rmax = Math.sqrt((x*x) + (y*y));
+				var theta = Math.atan (y/x) * (180/3.14159);
+				var phi = Math.atan (ry/rx) * (180/3.14159);
+				var fork = phi - theta; 
+				var d = (R * Math.sin (fork*(3.14159/180)));
+				var dabs = Math.abs(d);
+				if (dabs <= document.getElementById('projwidth').value  && R <= Rmax) 
+				{
+				console.log(d, theta, phi, fork);
+                        		var tranDots = L.circleMarker([this.geometry.coordinates[1], this.geometry.coordinates[0]], {
+                                        	radius: 3,
+                                        	fillColor: "black",
+                                        	color: "#000",
+                                        	weight: 1,
+                                        	opacity: 1,
+                                        	fillOpacity: 0.8
+                                	}).addTo(map);
+					tranDots;
+                		}
+	
+                		return this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value && dabs <= document.getElementById('projwidth').value;
+				});
+				};
+			});
+				
+		};
 
 
-	var myFilteredData = null;
+
+
+/*	var myFilteredData = null;
 
 	if (map.hasLayer(dots) && map.hasLayer(arrows)) {
 		map.removeLayer(dots);
 		map.removeLayer(arrows);
 	}
 
-	myFilteredTrans = $(myData).filter(function() {
+	if (mapclick.endLatlng != null) {
 		return this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value;
 	});
+	}
+
+	myFilteredTrans = $(myData).filter(function() {
 	myFilteredData = [].slice.call(myFilteredData);
         // Include data points
 	var overlayMaps = function (){
