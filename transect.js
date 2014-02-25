685e68e3ea9a9f9f413d6ee9aaeda7e832f340e4
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-02-25 12:10:30 -0500

    This version correctly grabs sites within a certain width

--- transect.js
+++ transect.js
@@ -1,13 +1,16 @@
-var dots = null,
-	arrows = null;
+//var dots = null,
+//	arrows = null;
 $("#submit").on("click", transect);
+///  Put array for transect
+// Array method called push  -- > push item into array 
 
 function transect() {
-         var mapclick = {
-                startLatlng: null,
-                endLatlng: null,
-                currentLine: null
-         };
+ var mapclick = {
+	startLatlng: null,
+	endLatlng: null,
+	currentLine: null,
+	allArray: []
+ };
 
 
         map.on('click', function(e) {
@@ -20,6 +23,9 @@ function transect() {
                         mapclick.currentLine = null;
                         mapclick.startLatlng1 = null;
                         mapclick.startLatlng2 = null;
+			$(mapclick.allArray).each(function() {
+				map.removeLayer(this);
+			});
                 }
                 if (mapclick.startLatlng === null) {
                         mapclick.startLatlng= e.latlng;
@@ -54,21 +60,28 @@ function transect() {
 				var rx = (mapclick.startLatlng.lng - this.geometry.coordinates[0])*111;
 				var ry = (mapclick.startLatlng.lat - this.geometry.coordinates[1])*111;
 			//		console.log(rx, mapclick.startLatlng.lng, this.geometry.coordinates[0]);
+				
+				if (mapclick.startLatlng.lat <= mapclick.endLatlng.lat)
+				{
 				var R = Math.sqrt((rx*rx) + (ry*ry));
 				var Rmax = Math.sqrt((x*x) + (y*y));
+				}
+				else
+				{
+				var R = -(Math.sqrt((rx*rx) + (ry*ry)));
+				var Rmax = -(Math.sqrt((x*x) + (y*y)));
+				}
 				var theta = Math.atan (y/x) * (180/3.14159);
 				var phi = Math.atan (ry/rx) * (180/3.14159);
 				var fork = phi - theta; 
 				var d = (R * Math.sin (fork*(3.14159/180)));
 				var dabs = Math.abs(d);
 				
-				        if (map.hasLayer(tranDots)) {
-         				       map.removeLayer(tranDots);
-       					 }
-				
-				if (dabs <= document.getElementById('projwidth').value  && R <= Rmax) 
+				if (mapclick.startLatlng.lat <= mapclick.endLatlng.lat && mapclick.startLatlng.lng <= mapclick.endLatlng.lng)		
 				{
-				console.log(d, theta, phi, fork);
+				if (dabs <= document.getElementById('projwidth').value  && R <= Rmax && this.geometry.coordinates[0] >= mapclick.startLatlng.lng && this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value) 
+				{
+				//console.log(d, theta, phi, fork);
                         		var tranDots = L.circleMarker([this.geometry.coordinates[1], this.geometry.coordinates[0]], {
                                         	radius: 3,
                                         	fillColor: "black",
@@ -76,12 +89,67 @@ function transect() {
                                         	weight: 1,
                                         	opacity: 1,
                                         	fillOpacity: 0.8
-                                	}).addTo(map);
-					tranDots;
+                                	})
+				mapclick.allArray.push(tranDots);
+				tranDots.addTo(map);
                 		}
-	
-               // 		return tranDots && this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value && dabs <= document.getElementById('projwidth').value;
+				}
+				else if (mapclick.startLatlng.lat >= mapclick.endLatlng.lat && mapclick.startLatlng.lng <= mapclick.endLatlng.lng)
+				{
+                                if (dabs <= document.getElementById('projwidth').value  && R >= Rmax && this.geometry.coordinates[0] >= mapclick.startLatlng.lng && this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value)
+                                {
+                                //console.log(d, theta, phi, fork);
+                                        var tranDots = L.circleMarker([this.geometry.coordinates[1], this.geometry.coordinates[0]], {
+                                                radius: 3,
+                                                fillColor: "black",
+                                                color: "#000",
+                                                weight: 1,
+                                                opacity: 1,
+                                                fillOpacity: 0.8
+                                        })
+                                mapclick.allArray.push(tranDots);
+                                tranDots.addTo(map);
+                                }
+                                }
+				else if (mapclick.startLatlng.lat >= mapclick.endLatlng.lat && mapclick.startLatlng.lng >= mapclick.endLatlng.lng)
+				{ 
+                                if (dabs <= document.getElementById('projwidth').value  && R >= Rmax && this.geometry.coordinates[0] <= mapclick.startLatlng.lng && this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value)
+                                {
+                                //console.log(d, theta, phi, fork);
+                                        var tranDots = L.circleMarker([this.geometry.coordinates[1], this.geometry.coordinates[0]], {
+                                                radius: 3,
+                                                fillColor: "black",
+                                                color: "#000",
+                                                weight: 1,
+                                                opacity: 1,
+                                                fillOpacity: 0.8
+                                        })
+                                mapclick.allArray.push(tranDots);
+                                tranDots.addTo(map);
+                                }
+                                }
+                                else
+                                { 
+                                if (dabs <= document.getElementById('projwidth').value  && R <= Rmax && this.geometry.coordinates[0] <= mapclick.startLatlng.lng && this.properties.svx <= document.getElementById('sigmax').value && this.properties.svy <= document.getElementById('sigmax').value)
+                                {
+                                //console.log(d, theta, phi, fork);
+                                        var tranDots = L.circleMarker([this.geometry.coordinates[1], this.geometry.coordinates[0]], {
+                                                radius: 3,
+                                                fillColor: "black",
+                                                color: "#000",
+                                                weight: 1,
+                                                opacity: 1,
+                                                fillOpacity: 0.8
+                                        })
+                                mapclick.allArray.push(tranDots);
+                                tranDots.addTo(map);
+                                }
+                                }
+
+
+
 				});
+			//	console.log(allarray);
 				};
 			});
 				
