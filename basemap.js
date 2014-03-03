e0deb54e3252e1858b1c5c019af5fae324607791
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-03-03 17:54:46 -0500

    This is the cleaned up version.  Uncertainties removed from plots.

--- basemap.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map_canvas').setView([35, -119.], 6);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-     maxZoom: 18
-}).addTo(map);
-L.tileLayer('http://geohazards.usgs.gov/ArcGIS/rest/services/qfaults2013/MapServer/tile/{z}/{y}/{x}', {
-     maxZoom: 18
-}).addTo(map);  
--- click.js
+++
@@ -1,7 +0,0 @@
-;function buttonClick(subEvent)
-{
-    var mainEvent = subEvent ? subEvent : window.event;
-
-    alert("This button click occurred at: X(" +
-    mainEvent.screenX + ") and Y(" + mainEvent.screenY + ")");
-}
